58ceb8d0350256c39e93e535143d859a167b18ec
krzysiek <krzyszotf.plonka64@gmail.com>
2023-11-07 21:32:26 +0100

    007 Enable Animation Update design to look better on mobile

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,10 +1,10 @@
-import { ComponentProps, ForwardedRef, ReactNode, forwardRef } from "react"
-import { Anchor as DefaultAnchor } from "@/components"
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import { faLink } from "@fortawesome/free-solid-svg-icons"
+import {ComponentProps, ForwardedRef, ReactNode, forwardRef} from "react"
+import {Anchor as DefaultAnchor} from "@/components"
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome"
+import {faLink} from "@fortawesome/free-solid-svg-icons"
 
 export const CardGroup = forwardRef((
-    { children, id }: { children: ReactNode, id?: string },
+    {children, id}: { children: ReactNode, id?: string },
     ref: ForwardedRef<HTMLDivElement>
 ) => (
     <section id={id} className="group/cards" ref={ref}>
@@ -12,44 +12,50 @@ export const CardGroup = forwardRef((
     </section>
 ))
 
-export function Container({ children }: { children: ReactNode }) {
-    return <div className="relative p-5 pl-36 transition-all duration-500 ease-in-out rounded-lg group hover:bg-slate-700/30  hover:!opacity-100 group-hover/cards:opacity-50">
+export function Container({children}: { children: ReactNode }) {
+    return <div
+        className="flex-col lg:relative mb-8 last:mb-0 pl-0 lg:pl-28 transition-all duration-500 ease-in-out group">
+        <div
+            className="pointer-events-none hidden lg:absolute -inset-4 rounded-lg group-hover:lg:bg-slate-700/40 hover:!opacity-100 group-hover/cards:opacity-50"></div>
         {children}
     </div>
 }
 
-export function Sidebar({ children }: { children: ReactNode }) {
-    return <span className="absolute text-xs font-light capitalize text-slate-500 left-5">{children}</span>
+export function Sidebar({children}: { children: ReactNode }) {
+    return <span
+        className="block lg:absolute text-xs font-light capitalize text-slate-500 left-0 lg:w-20 mb-1">{children}</span>
 }
 
-export function SidebarImage({ src, alt }: { src: string, alt: string }) {
-    return <img src={src} alt={alt} className="absolute object-cover w-24 h-16 rounded-md left-5" />
+export function SidebarImage({src, alt}: { src: string, alt: string }) {
+    return <img src={src} alt={alt}
+                className="hidden rounded-md lg:block lg:absolute object-cover w-36 lg:w-24 lg:h-16 lg:left-0"/>
 }
 
 
-export function Anchor({ children, href }: ComponentProps<typeof DefaultAnchor>) {
+export function Anchor({children, href}: ComponentProps<typeof DefaultAnchor>) {
     return <DefaultAnchor href={href} className="group-hover:text-lime-500">{children}</DefaultAnchor>
 }
 
-export function Content({ children }: { children: ReactNode }) {
-    return <span className="block">{children}</span>
+export function Content({children}: { children: ReactNode }) {
+    return <span className="block order-1">{children}</span>
 }
 
-export function Tags({ tags }: { tags: string[] }) {
+export function Tags({tags}: { tags: string[] }) {
     return <ul className="flex flex-wrap pt-3 gap-x-2 gap-y-2">
         {tags.map(tag => <Tag key={tag}>{tag}</Tag>)}
     </ul>
 }
 
-export function Tag({ children }: { children: ReactNode }) {
-    return <span className="px-2 py-1 text-xs font-medium rounded-lg text-lime-500 bg-lime-500/10 whitespace-nowrap">{children}</span>
+export function Tag({children}: { children: ReactNode }) {
+    return <span
+        className="px-2 py-1 text-xs font-medium rounded-lg text-lime-500 bg-lime-500/10 whitespace-nowrap">{children}</span>
 }
 
 export const WhiteAnchor = (props: ComponentProps<typeof Anchor>) => <DefaultAnchor className="text-slate-200" {...props} />
-export const HeaderAnchor = ({ href, children }: ComponentProps<typeof Anchor>) => (
+export const HeaderAnchor = ({href, children}: ComponentProps<typeof Anchor>) => (
     <DefaultAnchor href={href}>
         <h2 className="mb-2">
-            {children} <FontAwesomeIcon icon={faLink} className="ml-1" />
+            {children} <FontAwesomeIcon icon={faLink} className="ml-1"/>
         </h2>
     </DefaultAnchor>
 )
--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -1,4 +1,4 @@
-import { Card } from "@/components"
+import {Card} from "@/components"
 
 import EventMaster from "/EventMaster.png";
 import IntroSpotImg from "/IntroSpot.png";
@@ -7,16 +7,17 @@ import JungleGameImg from "/JungleGame.png";
 export function JungleGame() {
     return (
         <Card.Container>
-            <Card.SidebarImage src={JungleGameImg} alt="Placeholder" />
+            <Card.SidebarImage src={JungleGameImg} alt="Placeholder"/>
             <Card.Content>
                 <Card.HeaderAnchor href="https://github.com/ToJestKrzysio/TheJungleGame">Jungle Game</Card.HeaderAnchor>
                 <p className="text-sm text-slate-400">
                     Master of Science dissertation project. Application
-                    of Monte Carlo Tree Search with Value and Policy networks to predict best possible moves in the game of animal checkers.
+                    of Monte Carlo Tree Search with Value and Policy networks to predict best possible moves in the game
+                    of animal checkers.
                     User interface was created using react and backend was written in Flask.
                 </p>
             </Card.Content>
-            <Card.Tags tags={["Python", "Keras", "React", "Flask", "MCTS", "Reinforcement Learning", "Docker"]} />
+            <Card.Tags tags={["Python", "Keras", "React", "Flask", "MCTS", "Reinforcement Learning", "Docker"]}/>
         </Card.Container>
     )
 }
@@ -24,7 +25,7 @@ export function JungleGame() {
 export function EventManager() {
     return (
         <Card.Container>
-             <Card.SidebarImage src={EventMaster} alt="Placeholder" />
+            <Card.SidebarImage src={EventMaster} alt="Placeholder"/>
             <Card.Content>
                 <Card.HeaderAnchor href="https://github.com/ToJestKrzysio/EventMaster">Event Manager</Card.HeaderAnchor>
                 <p className="text-sm text-slate-400">
@@ -32,7 +33,7 @@ export function EventManager() {
                     Allows for login via identity providers (eg. google), and covers payment processing through PayU.
                 </p>
             </Card.Content>
-            <Card.Tags tags={["Python", "Django", "Bootstrap"]} />
+            <Card.Tags tags={["Python", "Django", "Bootstrap"]}/>
         </Card.Container>
     )
 }
@@ -40,14 +41,14 @@ export function EventManager() {
 export function IntroSpot() {
     return (
         <Card.Container>
-            <Card.SidebarImage src={IntroSpotImg} alt="IntroSpot thumbnail" />
+            <Card.SidebarImage src={IntroSpotImg} alt="IntroSpot thumbnail"/>
             <Card.Content>
                 <Card.HeaderAnchor href="https://github.com/ToJestKrzysio/Portfolio">IntroSpot</Card.HeaderAnchor>
                 <p className="text-sm text-slate-400">
-                    Basic portfolio site built with React and Tailwind.
+                    Simple portfolio site built with React and Tailwind.
                 </p>
             </Card.Content>
-            <Card.Tags tags={["React", "Typescript", "Tailwind CSS", "React Router", "AWS"]} />
+            <Card.Tags tags={["React", "Typescript", "Tailwind CSS", "React Router", "AWS"]}/>
         </Card.Container>
     )
 }
